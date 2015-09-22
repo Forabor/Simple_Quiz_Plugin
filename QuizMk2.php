99eb5e514d775e5699d57a268ac0d39d04ac2162
Corey Ferguson <forabor@yahoo.com>
2015-09-21 20:02:12 -0400

    Initial commit

---
+++ QuizMk2.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Plugin Name: Simple_Quiz_Plugin
+ * Description: Make and do a quiz.
+ * Plugin Author: Corey
+ * Version: 1.0.0
+ */
+
+define( 'CAF_PLUGIN_VER', '1.0.0');
+define('CAF_PLUGIN_URL', plugin_dir_url(__FILE__));
+
+add_action('wp_enqueue_scripts', 'quiz_Mk2');
+
+function quiz_Mk2(){
+    $page_id = get_the_ID();
+
+    wp_enqueue_script('caf-firebase', 'https://cdn.firebase.com/js/client/2.2.9/firebase.js', array('jquery'), CAF_PLUGIN_VER, false);
+
+    if ($page_id == 12){
+        wp_enqueue_script('caf-js-giver', CAF_PLUGIN_URL . 'assets/js/quizGiver.js', array('jquery', 'caf-firebase'), CAF_PLUGIN_VER, false);
+    }
+
+    if($page_id == 17){
+        wp_enqueue_script('caf-js-taker', CAF_PLUGIN_URL . 'assets/js/quizTaker.js', array('jquery', 'caf-firebase'), CAF_PLUGIN_VER, false);
+    }
+
+    wp_enqueue_style('car-css', CAF_PLUGIN_URL . 'assets/css/styles.css');
+}
+
+//quizGiver
+add_shortcode( 'quizgiver', 'caf_add_quizGiver_shortcode');
+
+function caf_add_quizGiver_shortcode(){
+    $content = <<<QUIZGIVER
+
+
+<h1><input type="text" id="quizName" class="quizName" placeholder="Enter your quiz name"/></h1>
+
+<ol id="inputQuestions">
+    <li>
+        <input type="text" id="question0" placeholder="Enter your question here."/> <input type="text" id="answer0"
+                                                                                           placeholder="Enter the answer here."/>
+    </li>
+
+</ol>
+
+<p onclick="addQuestionInput();" class="newQuestionButton" id="newQuestionButton">New Question.</p>
+
+<p onclick="add10Questions();" class="newQuestionButton10" id="newQuestionButton10">Add 10 Questions.</p>
+
+<p onclick="addXQuestions();" class="newQuestionButtonX" id="newQuestionButtonX">Add Multiple Questions.</p>
+</br>
+<p onClick="submitQuestions();" class="submitQuestionButton" id="submitQuestionButton">Submit</p>
+
+
+QUIZGIVER;
+
+    return $content;
+}
+
+add_shortcode('quiztaker', 'caf_add_quizTaker_shortcode');
+
+function caf_add_quizTaker_shortcode(){
+    $content = <<<QUIZTAKER
+
+
+<h1>Quiz</h1>
+
+<p onclick="takeQuiz();" class="quizButton" id="quizButton">Take Quiz?</p>
+
+
+<ol id="allQuestions"></ol>
+
+
+<p id="hiddenButton"></p>
+
+<p id="resultsCorrect"></p>
+
+<p id="resultsIncorrect"></p>
+
+QUIZTAKER;
+    return $content;
+}
---
+++ assets/js/quizTaker.js
@@ -0,0 +1,131 @@
+var questions = [];
+var numOfGivenAnswers = 0;
+var numQuestionsCorrect = 0;
+var numQuestionsIncorrect = 0;
+var questionsCorrect = [];
+var questionsIncorrect = [];
+
+//Adds a <li> to <ol>"allQuestions" and adds an input text box
+function appendOL(questionContent) {
+
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(questionContent);
+    var breaknode = document.createElement("br");
+    var inputNode = document.createElement("input");
+    node.appendChild(textnode);
+    node.appendChild(breaknode);
+    node.appendChild(inputNode);
+    document.getElementById("allQuestions").appendChild(node);
+    inputNode.setAttribute("type", "text");
+    inputNode.setAttribute("id", "givenAnswer" + i);
+    numOfGivenAnswers += 1;
+}
+
+//Hides the quiz button after user agrees to take test.
+function hideButton() {
+    document.getElementById("quizButton").style.display = "none";
+}
+
+//Adds Submit Button after use agrees to take test.
+function addSubmit() {
+    var textnode = document.createTextNode("Submit");
+    document.getElementById("hiddenButton").setAttribute("onClick", "submitAnswers();");
+    document.getElementById("hiddenButton").setAttribute("class", "submitAnswersButton");
+    document.getElementById("hiddenButton").appendChild(textnode);
+}
+
+//Disables HTML Function
+function disable(htmlstuff) {
+    document.getElementById(htmlstuff).style.pointerEvents = "none";
+}
+
+
+function pushHTML(id, content) {
+    document.getElementById(id).innerHTML = content
+}
+
+
+//Creates an ordered list of questions incorrect
+function createIncorrectList() {
+    var resultsIncorrect = "<h3>The questions you got incorrect were:</h3><ol>";
+    for (i = 0; i < questionsIncorrect.length; i++) {
+
+        var x = questionsIncorrect[i].question;
+
+        resultsIncorrect += "<li>" + questionsIncorrect[i].question + "</li>";
+        resultsIncorrect += "<ul><li style='color: red'>You answered " + questionsIncorrect[i].given + "</li>" + "<li style='color: green'>The correct answer was " + questionsIncorrect[i].answer + ".</li></ul>";
+    }
+    resultsIncorrect += "</ol>";
+    pushHTML("resultsIncorrect", resultsIncorrect);
+}
+
+//Creates an ordered list of questions coorrect
+function createCorrectList() {
+    var resultsCorrect = "<h3>The questions you got correct were:</h3><ol>";
+    for (i = 0; i < questionsCorrect.length; i++) {
+
+        var x = questionsCorrect[i].question;
+
+        resultsCorrect += "<li>" + questionsCorrect[i].question + "</li>";
+        resultsCorrect += "<ul><li style='color: red'>You answered " + questionsCorrect[i].given + "</li>" + "<li style='color: green'>The correct answer was " + questionsCorrect[i].answer + ".</li></ul>";
+    }
+    resultsCorrect += "</ol>";
+    pushHTML("resultsCorrect", resultsCorrect);
+}
+
+//What happens when the user clicks the submit button.
+function submitAnswers() {
+    for (i = 0; i < numOfGivenAnswers; i++) {
+
+        disable("givenAnswer" +i);
+        disable("hiddenButton");
+        var x = document.getElementById("givenAnswer" + i).value;
+        var y = questions[i].answer;
+        var z = questions[i].question;
+        questions[i].given = x;
+        var g = questions[i].given;
+
+        if (y === x) {
+            numQuestionsCorrect += 1;
+            questionsCorrect.push({question: z, answer: y, given: g});
+        } else {
+            numQuestionsIncorrect += 1;
+            questionsIncorrect.push({question: z, answer: y, given: g});
+        }
+    }
+
+    if (questionsCorrect.length > -1) {
+        createCorrectList();
+    }
+
+    if (questionsIncorrect.length > -1) {
+        createIncorrectList();
+    }
+
+}
+
+//If user agrees to take quiz. Generate questions from firebase that has been submitted by QuizGiver.
+function takeQuiz() {
+
+    var quizChoice = prompt("What quiz would you like to take?");
+    var myFirebaseRef = new Firebase("https://boiling-inferno-3250.firebaseio.com/" + quizChoice.toLowerCase());
+    var decision = prompt("Are you sure the quiz you wanted to take was " + quizChoice + "?" +
+    " Type 'yes' to proceed, type 'no' to choose a new quiz,  or type 'cancel' to cancel.");
+
+    if (decision.toLowerCase() === "yes") {
+        myFirebaseRef.on("value", function (snapshot) {
+            console.log(snapshot.val());
+            questions = snapshot.val();
+            for (i = 0; i < questions.length; i++) {
+                appendOL(questions[i].question);
+            }
+            hideButton();
+            addSubmit();
+
+        });
+    } else if(decision.toLowerCase() == "no"){
+
+        takeQuiz();
+
+    }
+}
